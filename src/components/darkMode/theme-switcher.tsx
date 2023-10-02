20db7e35ad918d886a13a598e19843b9da59a408
Julian Valle <jvalle092@gmail.com>
2023-10-01 22:11:07 -0400

    dark mode completed

---
+++ src/components/darkMode/theme-switcher.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+export const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      className={`duration-20 rounded px-7 py-2 transition ease-out hover:scale-110 active:scale-100 dark:bg-[#212933]`}
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+    >
+      {theme === "light" ? "Dark" : "Light"}
+    </button>
+  );
+};
---
+++ src/components/navBar/navbar.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { ThemeSwitcher } from "../darkMode/theme-switcher";
+
+export const Navbar = () => {
+  return (
+    <header className="py-2">
+      <nav className="container mx-auto">
+        <div className="hidden justify-between md:flex">
+          <Link href="/" className="px-4 py-6">
+            Julian Valle
+          </Link>
+          <div className="flex w-1/3 items-center justify-evenly">
+            <Link
+              href="/"
+              className="rounded px-4 py-6 transition ease-in-out hover:scale-110 active:scale-100 dark:hover:bg-purple-600"
+            >
+              About
+            </Link>
+            <Link
+              href="/"
+              className="rounded px-4 py-6 duration-200 hover:scale-110 active:scale-100 dark:hover:bg-purple-600"
+            >
+              Projects
+            </Link>
+            <Link
+              href="/"
+              className="rounded px-4 py-6 duration-200 hover:scale-110 active:scale-100 dark:hover:bg-purple-600"
+            >
+              Contact
+            </Link>
+            <ThemeSwitcher />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
