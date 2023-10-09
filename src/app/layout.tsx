f7183471ca79082cdc3aebd129f2af78cae60db3
Julian Valle <jvalle092@gmail.com>
2023-10-09 18:29:11 -0400

    analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import { config, library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import { Analytics } from "@vercel/analytics/react";
+
 config.autoAddCss = false;
 
 library.add(fas, far, fab);
@@ -32,6 +34,7 @@ export default function RootLayout({
         <Providers attribute="class">
           <Navbar />
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
