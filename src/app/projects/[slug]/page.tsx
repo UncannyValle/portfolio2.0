aea70105687629c9493dc82e2f0a1fa5b2a3cc8a
Julian Valle <jvalle092@gmail.com>
2023-10-08 22:06:03 -0400

    slight changes to links

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,5 +1,9 @@
 import prisma from "@/utils/prisma";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+
+export const revalidate = 3;
 
 const getProject = async (slug: string) => {
   return await prisma.project.findUnique({
@@ -18,7 +22,7 @@ export default async function ProjectPage({
 
   return (
     <main className="container mx-auto mt-[120px] min-h-screen">
-      <h1 className="text-8xl">{project?.title}</h1>
+      <h1 className="text-8xl text-center">{project?.title}</h1>
       <Image
         src={`/images/projects/${project?.slug}/main.png`}
         alt="project picture"
@@ -27,6 +31,22 @@ export default async function ProjectPage({
         width={1200}
       />
       <div className="mx-auto my-8 w-3/4">
+        {project?.gtihub ? (
+          <a
+            href={project.gtihub}
+            className="text-4xl text-purple-600 underline"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Check Out The Repo
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        ) : (
+          <p className="text-4xl text-purple-600">
+            Private Repo
+            <FontAwesomeIcon className="px-4" icon={faGithub} />
+          </p>
+        )}
         <div className="my-8 ">
           <h2 className="py-8 text-4xl uppercase">Summary:</h2>
           <hr className="my-8 h-1 border-0 bg-slate-800" />
